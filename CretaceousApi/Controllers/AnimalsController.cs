aa63372f3f4517cdaa2c0593c0543d70c9b333c4
Zachary <ZackCipolletta@gmail.com>
2023-03-26 13:11:03 -0700

    complete full CRUD functionality

--- CretaceousApi/Controllers/AnimalsController.cs
+++ CretaceousApi/Controllers/AnimalsController.cs
@@ -6,21 +6,23 @@ namespace CretaceousApi.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class ANimlasController : ControllerBase
+  public class AnimalsController : ControllerBase
   {
     private readonly CretaceousApiContext _db;
 
-    public ANimlasController(CretaceousApiContext db)
+    public AnimalsController(CretaceousApiContext db)
     {
       _db = db;
     }
 
+    // GET api/animals
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Animal>>> Get()
     {
       return await _db.Animals.ToListAsync();
     }
 
+    // GET: api/Animals/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Animal>> GetAnimal(int id)
     {
@@ -32,6 +34,67 @@ namespace CretaceousApi.Controllers
       }
 
       return animal;
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+
+    // PUT: api/Animals/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Animals.Update(animal);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
+        // DELETE: api/Animals/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
     }
   }
 }
